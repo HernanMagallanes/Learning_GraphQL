8c5de8472689e530b0a8657892b5b3ee30d46d3a
Hernán M <hernan.magallanes.94@gmail.com>
2022-07-28 08:58:50 -0300

    add createUser and login mutations

--- config.js
+++ config.js
@@ -6,3 +6,5 @@ export const MONGODB_URI =
 	process.env.MONGODB_URI || "mongodb://localhost/graphQL_db";
 
 export const PORT = process.env.PORT || 5000;
+
+export const JWT_SECRET = process.env.JWT_SECRET;
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import { gql, ApolloServer, UserInputError } from "apollo-server";
 
 import "./db.js";
 import Person from "./models/person.js";
+import User from "./models/user.js";
+import jwt from "jsonwebtoken";
+
+import { JWT_SECRET } from "./config.js";
 
 // data description / definition
 
@@ -24,10 +28,21 @@ const typeDefs = gql`
 		id: ID!
 	}
 
+	type User {
+		username: String!
+		friends: [Person]!
+		id: ID!
+	}
+
+	type Token {
+		value: String!
+	}
+
 	type Query {
 		personCount: Int!
 		allPersons(phone: YesNo): [Person]!
 		findPerson(name: String!): Person
+		me: User
 	}
 
 	type Mutation {
@@ -39,6 +54,9 @@ const typeDefs = gql`
 		): Person
 
 		editNumber(name: String!, phone: String!): Person
+
+		createUser(username: String!): User
+		login(username: String!, password: String!): Token
 	}
 `;
 
@@ -52,9 +70,16 @@ const resolvers = {
 			if (!args.phone) return Person.find({});
 			return Person.find({ phone: { $exists: args.phone === "YES" } });
 		},
+
 		findPerson: (root, args) => {
 			const { name } = args;
 			return Person.findOne({ name });
+		},
+
+		me: (root, args, context) => {
+			console.log("me query OK");
+
+			return context.currentUser;
 		},
 	},
 
@@ -86,6 +111,38 @@ const resolvers = {
 
 			return person;
 		},
+
+		createUser: (root, args) => {
+			const user = new User({ username: args.username });
+
+			console.log("createUser OK");
+
+			return user.save().catch((error) => {
+				throw new UserInputError(error.message, {
+					invalidArgs: args,
+				});
+			});
+		},
+
+		login: async (root, args) => {
+			const user = await User.findOne({ username: args.username });
+
+			console.log("login OK");
+
+			// unique password !
+			if (!user || args.password != "secret") {
+				throw new UserInputError("wrong credentials");
+			}
+
+			const userForToken = {
+				username: user.username,
+				id: user._id,
+			};
+
+			return {
+				value: jwt.sign(userForToken, JWT_SECRET),
+			};
+		},
 	},
 
 	Person: {
@@ -105,6 +162,18 @@ const resolvers = {
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
+
+	context: async ({ req }) => {
+		const auth = req ? req.headers.authorization : null;
+
+		if (auth && auth.toLowerCase().startsWith("bearer ")) {
+			const token = auth.substring(7);
+			const { id } = jwt.verify(token, JWT_SECRET);
+			const currentUser = await User.findById(id).populate("friends");
+
+			return { currentUser };
+		}
+	},
 });
 
 server.listen().then(({ url }) => {
